f0b936e61a3d004d571f98998f695d96840a2a53
S M Piyas Mahamude Alif <piyasmahmudealif@gmail.com>
2024-08-01 01:42:07 +0600

    delete all temp val of upload folder

--- src/app/modules/AUTH/auth.services.ts
+++ src/app/modules/AUTH/auth.services.ts
@@ -35,7 +35,7 @@ const Auth_Login_Service = async (gettedData: Create_Token_Data_Type) => {
 
 const Refresh_Token_Service = async (token: string) => {
     const decodedData = Decode_Token(token) as JwtPayload;
-    console.log({token,decodedData});
+    console.log({ token, decodedData });
     const isUserExist = await User_Model.findOne({ email: decodedData.email });
     if (!isUserExist) {
         throw new Final_App_Error(httpStatus.NOT_FOUND, "User Not Found !");
@@ -45,7 +45,7 @@ const Refresh_Token_Service = async (token: string) => {
         email: isUserExist.email
     }, '1hr')
 
-    return { AccessToken,userId:isUserExist._id }
+    return { AccessToken, userId: isUserExist._id }
 }
 
 const Change_Password_Service = async (gettedData: Change_Password_Data_Type, token: string) => {
@@ -91,35 +91,43 @@ const Forget_Password_Service = async (email: string) => {
         email: isEmailExist.email
     }, '5m')
     // if user found then send a link to user email, when user click it then it will nagivate the user to set a new password page
-    const forgetPass_url = `${config.client_url}/auth/reset?userEmail=${isEmailExist.email}&token=${resetToken}`;
-    SendEmail(isEmailExist.email, `<h1>Click The Link To Change Your Password</h1><br><a href="${forgetPass_url}">Click Here 🤩</a>`, "Forget Your CODE_NOTE Password");
+    const forgetPass_url = `${config.client_url}/reset?email=${isEmailExist.email}&token=${resetToken}`;
+    try {
+        SendEmail(isEmailExist.email, `<h1>Click The Link To Change Your Password</h1><br><a href="${forgetPass_url}">Click Here 🤩</a>`, "Forget Your CODE_NOTE Password");
+    } catch (err) {
+        console.log(err);
+    }
 
     return "Email Send Successfully form the server !"
 }
 
 const Reset_Password_Service = async (gettedData: Reset_Password_Type, token: string) => {
     // check if the user is exist by the email
-    const user = await User_Model.findOne({ email:gettedData.email });
+    const user = await User_Model.findOne({ email: gettedData.email });
     if (!user) {
         throw new Final_App_Error(httpStatus.NOT_FOUND, "User Not Found *");
     }
     const decodedTokenData = Decode_Token(token) as JwtPayload;
-    if(decodedTokenData.email !== user.email){
-        throw new Final_App_Error(httpStatus.UNAUTHORIZED,"Unauthorized Access *");
+    if (decodedTokenData.email !== user.email) {
+        throw new Final_App_Error(httpStatus.UNAUTHORIZED, "Unauthorized Access *");
     }
 
     const encodeNewPassword = await Encrypt_Password(gettedData.newPassword);
 
-    const result = await User_Model.findByIdAndUpdate({_id:user._id},{
-        password:encodeNewPassword,
-        passwordChangeAt:new Date
-    },{new:true})
+    const result = await User_Model.findByIdAndUpdate({ _id: user._id }, {
+        password: encodeNewPassword,
+        passwordChangeAt: new Date
+    }, { new: true })
 
 
     const to = user.email;
     const html = `<h1>Someone reset your CODE_NOTE account password</h1><br><p>If, it's not you then come to CODE_NOTE and secure your account</p><br><p>Or If it's you then avoid this email and carry on !!</p>`
     const subject = "Your CODE_NOTE account password reset successful !";
-    SendEmail(to,html,subject);
+    try {
+        SendEmail(to, html, subject);
+    } catch (err) {
+        console.log(err);
+    }
 
     return result;
 }
